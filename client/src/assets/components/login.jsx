f2e208cc73b4f4aa81da769110a75e25b5cd88bd
Ellie Racheeva <ellie.racheeva@gmail.com>
2024-01-08 13:16:01 +0000

    updates

--- client/src/assets/components/login.jsx
+++ client/src/assets/components/login.jsx
@@ -1,10 +1,32 @@
-import React, { useState } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import "./login.css";
 
 function Login() {
-  return (<div class="registration">
-  </div>
-  );
+  const userRef = useRef();
+  const errRef = useRef();
+
+  const [user, setUser] = useState('');
+  const [password, setPassword] = useState('');
+  const [errMsg, setErrMsg] = useState('');
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+    userRef.current.focus();
+
+  }, [])
+
+  useEffect(() => {
+    setErrMsg('');
+  }, [user, password])
+
+  return (
+    
+    <div>
+    
+
+
+
+  </div>);
 }
 
 export default Login;
