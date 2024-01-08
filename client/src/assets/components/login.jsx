65142161a278582486000a6f3a55ca55a5250b0f
elli-elka <71603746+elli-elka@users.noreply.github.com>
2024-01-08 17:05:47 +0000

    changes to login page

--- client/src/assets/components/login.jsx
+++ client/src/assets/components/login.jsx
@@ -22,7 +22,17 @@ function Login() {
   return (
     
     <div>
-    
+    <p ref-{errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
+    <h1>Sign in</h1>
+    <form>
+    <label htmlFor="username">Username:</label>
+    <input type="text" id="username" ref={userRef} autoComplete ="off" onChange={(e) => setUser(e.target.value)} value = {user} required />
+
+    <label htmlFor="password">Password:</label>
+    <input type="password" id="password" ref={userRef} autoComplete ="off" onChange={(e) => setPassword(e.target.value)} value = {password} required />
+
+    <button>Sign in</button>
+    </form>
 
 
 
