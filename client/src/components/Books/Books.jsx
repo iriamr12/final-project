e93c9f5bff5b1c92e0ac52ca96f50b53fa40e6f2
Ellie Racheeva <ellie.racheeva@gmail.com>
2024-01-30 22:53:00 +0000

    css fiddling

--- client/src/components/Books/Books.jsx
+++ client/src/components/Books/Books.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import "./Books.css";
 
-export default function Books({data}) {
-    return (
-      <div className="books-container">
-        {data.map((book, index) => (
-          <div className="each-book" key={index}>
+export default function Books({ data }) {
+  return (
+    <div className="book-list">
+      {data.map((book, index) => (
+          <div key={index} className="books">
+            <h3 className="titleName">{book.volumeInfo.title}</h3>
+            <p className="title">TITLE</p>
+            {/*TODO this line needs some work, as it emits errors*/}
+            {/*<p>{book.volumeInfo.authors.join(", ")}</p>*/}
+            <p className="authors">AUTHORS</p>
+
             <a href={book.volumeInfo.previewLink}>
-            <img src={book?.volumeInfo?.imageLinks?.smallThumbnail}/>
+              <img className="image" src={book?.volumeInfo?.imageLinks?.smallThumbnail}/>
             </a>
-            <h3>{book.volumeInfo.title}</h3>
-            {/* <p>Authors: {book.volumeInfo.authors.join(', ')}</p> */}
+            {/*book.volumeInfo.industryIdentifiers[0].identifier*/}
+            <br/>
+            <button className="viewer" onClick={() => window.location.href = `/bookviewer?isbn=9781291512472`}>embedded viewer</button>
           </div>
-        ))}
-      </div>
-    );
-  }
-  
+      ))}
+    </div>
+  );
+}
