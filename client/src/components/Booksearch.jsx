21b7a842e03da390b712089559ef2a964105ebb4
Iria Moreno Rodriguez <iria.moreno12@gmail.com>
2024-02-01 00:03:30 +0100

    homepageupdate

--- client/src/components/Booksearch.jsx
+++ client/src/components/Booksearch.jsx
@@ -1,11 +1,13 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Books from "./Books/Books";
+import Type from "./Type/Type";
 import BookForm from "./BookForm";
 import "./Booksearch.css";
 
 function BookSearch() {
   const [loading, setLoading] = useState(false);
   const [books, setBooks] = useState([]);
+  const [booksType, setBooksType] = useState([]);
   const [error, setError] = useState("");
 
   const API_KEY = "AIzaSyDBN0k-cEvlnsLQj0YELqLD1ckFYMmPF4I";
@@ -32,6 +34,37 @@ function BookSearch() {
     setLoading(false);
   };
 
+  const getBookByType = async () => {
+    setLoading(true);
+    setError("");
+
+    const maxResults = 40;
+
+    let url1 = `https://www.googleapis.com/books/v1/volumes?q={"fiction","education","cooking"}&key=${API_KEY}&maxResults=${maxResults}`;
+   
+    try {
+        let response = await fetch(url1);
+        if (response.ok) {
+            let data1 = await response.json();
+            console.log('We got data by type!');
+            console.log(data1);
+            setBooksType(data1.items);
+        } else {
+            setError(`Server Error: ${response.status} ${response.statusText}`);
+        }
+    } catch (e) {
+        setError(`Network Error: ${e.message}`);
+    }
+    setLoading(false);
+};
+
+
+  useEffect(() => {
+    // Call getBookByType when the component mounts
+    getBookByType();
+  }, []); // The empty dependency array ensures that this effect runs only once on mount
+
+
   return (
     <>
       <div className="containers">
@@ -40,17 +73,14 @@ function BookSearch() {
         <Books data={books} />
         {loading && <h2>Loading...</h2>}
         {error && <h2 style={{ color: "red" }}>{error}</h2>}
-        
-          
-
-
-
-
-        
-        
         </div>
-        
-        
+      <div>
+        {booksType.length > 0 ? (
+          <Type data1={booksType}/>
+        ) : (
+          <h2>No data available for Type</h2>
+        )}        
+        </div>
       </div>
     </>
   );
